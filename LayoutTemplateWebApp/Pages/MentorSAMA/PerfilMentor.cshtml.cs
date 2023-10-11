773cb465e0789ebf8361c45e4a7e6fe8c892842d
MarioLara21 <mariolara.21@estudianTEC.cr>
2023-10-10 21:12:06 -0600

    Avance API

--- LayoutTemplateWebApp/Pages/MentorSAMA/PerfilMentor.cshtml.cs
+++ LayoutTemplateWebApp/Pages/MentorSAMA/PerfilMentor.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 
 namespace LayoutTemplateWebApp.Pages.MentorSAMA
 {
     public class PerfilMentorModel : PageModel
     {
+        private static readonly String connApi = "http://sistema-tec.somee.com/api/degree";
+        public async Task OnGetApi()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(connApi))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(apiResponse);
+                    }
+                }
+            }
+        }
         public void OnGet()
         {
+            OnGetApi();
         }
+        
     }
 }
